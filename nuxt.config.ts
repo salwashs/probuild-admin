a01ddce40037bdcf2353738d748c5a38b16adc47
Salwa Salsabila <salwa2105salsabila@gmail.com>
2026-04-05 22:32:31 +0800

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui',
-    '@vueuse/nuxt',
-    '@sentry/nuxt/module'
-  ],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@vueuse/nuxt", "@sentry/nuxt/module"],
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   routeRules: {
-    '/api/**': {
-      cors: true
-    }
+    "/api/**": {
+      cors: true,
+    },
   },
 
-  compatibilityDate: '2024-07-11',
+  compatibilityDate: "2024-07-11",
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
   },
 
   sentry: {
-    org: 'none-m5k',
-    project: 'probuild-admin'
+    org: "none-m5k",
+    project: "probuild-admin",
+    autoInjectServerSentry: "top-level-import",
   },
 
   sourcemap: {
-    client: 'hidden'
-  }
-})
+    client: "hidden",
+    server: false,
+  },
+});
